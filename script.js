ecee971bb13209cc091fca981bcf2a462f431177
Eunoia-12 <adrianadsouza0405@gmail.com>
2024-11-04 17:34:18 -0500

    changing the html file name

--- script.js
+++ script.js
@@ -5,21 +5,3 @@ function toggleMenu(){
     icon.classList.toggle("open")
 }
 
-const imgs = document.querySelectorAll('.slider img')
-const dots = document.querySelectorAll('.dot')
-let currentImg = 0
-const interval = 3000
-
-function changeSlide(n){
-    for (let i = 0; i < imgs.length; i++){
-        imgs[i].style.opacity = 0
-        dots[i].className = dots[i].className.replace('active','')
-    }
-
-    currentImg = n
-    
-    imgs[currentImg].style.opacity = 1
-    dots[currentImg].className += 'active'
-}
-
-
